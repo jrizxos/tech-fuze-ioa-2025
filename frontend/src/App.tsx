1ab5a0411b8ea7d59e5cbbae5e1baf2cee0f58bc
jrizxos <jrizos98@gmail.com>
2025-03-15 13:56:42 +0200

    Finished

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,50 +17,115 @@ const xoff = 24.7;
 const yoff = 36.7;
 
 export const points: MapPoint[] = [
+  {x: 100+xoff*1,
+    y: 100+xoff*-1.5,
+    data: 1
+   },
   {x: 100+xoff*0,
    y: 100+xoff*0,
-   data: 1
+   data: 10
   },
+  {x: 100+xoff*5,
+    y: 100+xoff*4.5,
+    data: 10
+   },
+   {x: 100+xoff*6,
+    y: 100+xoff*6,
+    data: 10
+   },
+   {x: 100+xoff*7,
+    y: 100+xoff*7.5,
+    data: 10
+   },
+   {x: 100+xoff*8,
+    y: 100+xoff*9,
+    data: 10
+   },
+   
+   {x: 100+xoff*6,
+    y: 100+xoff*3,
+    data: 37
+   },
+   {x: 100+xoff*7,
+    y: 100+xoff*4.5,
+    data: 37
+   },
+   {x: 100+xoff*8,
+    y: 100+xoff*6,
+    data: 37
+   },
+   {x: 100+xoff*9,
+    y: 100+xoff*7.5,
+    data: 37
+   },
+   {x: 100+xoff*10,
+    y: 100+xoff*9,
+    data: 37
+   },
+
+   {x: 100+xoff*8,
+    y: 100+xoff*3,
+    data: 55
+   },
+   {x: 100+xoff*9,
+    y: 100+xoff*4.5,
+    data: 55
+   },
+   {x: 100+xoff*10,
+    y: 100+xoff*6,
+    data: 300
+   },
+   {x: 100+xoff*11,
+    y: 100+xoff*7.5,
+    data: 300
+   },
+   {x: 100+xoff*12,
+    y: 100+xoff*9,
+    data: 37
+   },
+
   {x: 100+xoff*1,
    y: 100+yoff*1,
-   data: 2
+   data: 20
   },
   {x: 100+xoff*2,
    y: 100+yoff*2,
-   data: 3
+   data: 41
   },
   {x: 100+xoff*3,
     y: 100+yoff*1,
-    data: 3
+    data: 120
    },
    {x: 100+xoff*4,
     y: 100+yoff*2,
-    data: 3
+    data: 150
    },
    {x: 100+xoff*4,
     y: 100+yoff*0,
-    data: 2
+    data: 40
    },
    {x: 100+xoff*2,
     y: 100+yoff*0,
-    data: 2
+    data: 29
    },
    {x: 100+xoff*5,
     y: 100+yoff*1,
-    data: 2
+    data: 27
    },
   {x: 100+xoff*1,
     y: 100+yoff*3,
-    data: 4
+    data: 350
    },
   {x: 100+xoff*3,
   y: 100+yoff*3,
-  data: 4
+  data: 350
   },
   {x: 100+xoff*0,
     y: 100+yoff*2,
-    data: 4
+    data: 350
    },
+
+
 
 ];
 
@@ -82,16 +147,16 @@ function App() {
               {
                 return (
                   <>
-                    <img src={hex} className="map-point" 
-                    key={index}
-                    style={{top: point.y, left: point.x}}/>
-                    <img src={point.data==1?hexg:
-                              point.data==2?hexy:
-                              point.data==3?hexo:
+                   <img src={(point.data<25)?hexg:
+                             (25<=point.data && point.data<50)?hexy:
+                             (50<=point.data && point.data<200)?hexo:
                               hexr
                     } className="map-point" 
                     key={index}
-                    style={{top: point.y, left: point.x, opacity: "0.5"}}/>
+                    style={{top: point.y, left: point.x, opacity: "0.35"}}/>
+                    <img src={hex} className="map-point" 
+                    key={index}
+                    style={{top: point.y, left: point.x, opacity: "0.75"}}/>
                   </>
                 )
               }
